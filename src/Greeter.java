a31f251816b631496921228d46e626ba739a38a6
mridulwaldia <mridulsinghwaldia@gmail.com>
2018-02-16 11:55:39 -0600

    Merge branch 'TestClass' into dev

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,4 +1,20 @@
 
 public class Greeter {
 
+	private String name;
+	private int age;
+public Greeter(){
+	
 }
+
+public Greeter(String xName, int xAge){
+	name = xName;
+	age = xAge;
+}
+
+public String toString(){
+	return "Greeter" + "Name: " + name + "Age: " + age;
+}
+}
+
+
--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,8 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Greeter bob = new Greeter();
+		System.out.println(bob.toString());
 	}
 
 }
